065f5293f4147bc21f6c8487d208ee72b0f3cf4f
Pierre Massat <pmassat@stanford.edu>
2019-03-06 14:28:34 -0800

    Neutrons v1.3.6. Modified code that extracts position of peak maxima.

--- Matlab_data_analysis/Neutrons/ENS_peak_fit_ICpV/ENS_peak_fit_ICpV.m
+++ Matlab_data_analysis/Neutrons/ENS_peak_fit_ICpV/ENS_peak_fit_ICpV.m
@@ -16,48 +16,56 @@ hh1 = nDatap94(i).hh0;
 I1dat = nDatap94(i).I;
 dI = nDatap94(i).dI;
 %%
-istart = 31;%length(H);
-iend = 45;
+istart = 1;
+iend = length(H);
 %% Fit single Ikeda-Carpenter-pseudo-Voigt peak at high field
 % Analysis parameters
 hc = -8.0;% center of unsplit peak in reciprocal space
 % ufb = 0.99; % upper fit boundary = highest value of h-hc for which to include datapoints for the fit
 field = H;%cell2mat( arrayfun(@(c) c.field, nDatap94(1:istart).', 'Uniform', 0) );
-datExcld = nDatap94(istart).hh0<min(hc)-.7 | nDatap94(istart).hh0>max(hc)+0.55 |...
-    (nDatap94(istart).hh0>min(hc)-.35 & nDatap94(istart).hh0<min(hc)-0.15)...
-     | (nDatap94(istart).hh0>max(hc)+0.14 & nDatap94(istart).hh0<max(hc)+0.2);% Exclude 
+nData1 = nDatap94(istart).hh0;
+datExcld = nData1<min(hc)-.7 | nData1>max(hc)+0.55 |...
+    (nData1>min(hc)-.35 & nData1<min(hc)-0.15) |...
+    (nData1>max(hc)+0.14 & nData1<max(hc)+0.2);% Exclude  |...
+%     (nData1>max(hc)+0.045 & nData1<max(hc)+0.065);% Exclude 
 % data points that correspond to other peaks as well as those that are too far away
 
 %% Perform and plot fit
 xc = [-8.03 -7.99];% position of peaks in reciprocal space
 lx = length(xc);
-rng = istart:1:iend;
+rng = 46:54%istart:1:iend;
 I1 = 1e5; R1 = 0.1; a1 = 200; b1 = 0.1; g1 = 1e-3; s1 = 6.6e-3;% free parameters initial values
 I2 = 2e5; R2 = 0.1; a2 = 200; b2 = 0.1; g2 = 1e-3; s2 = 6.6e-3;% free parameters initial values
 % freePrms1 = {'I',I1;'R',R1;'alpha',a1;'beta',b1;'gamma',g1;'sigma',s1;'x0',hc};%7 free parameters
 % freePrms1 = {'I1',I1;'R1',R1;'beta1',b1;'gamma1',g1;'x01',hc(1)};% 5 free parameters
 % freePrms2 = {'I2',I2;'R2',R2;'beta2',b2;'gamma2',g2;'x02',hc(2)};% 5 free parameters
 freePrms1 = {'I1',I1;'x01',xc(1)};% 3 free parameters
-freePrms2 = {'I2',I2;'gamma2',g2;'x02',xc(2)};% 3 free parameters
+freePrms2 = {'I2',I2;'x02',xc(2)};% 3 free parameters
 % Note: the order in which free parameters are defined matters because of
 % how the array of initial fitting parameters 'initParams' is defined
+fmt = 'ENS pattern along [hh0] T=%.2fK H=%.3fT %i params fit';% string format for plot title
 for i=rng
     myfit = fitICpV(nDatap94(i).hh0,nDatap94(i).I,xc); 
     myfit.dataExcl = datExcld;
     ap1 = myfit.allParams{1}; ap1('alpha') = 140; ap1('sigma') = 6.6e-3;% 'ap1' is shorter than 'myfit.allParams{1}'
     ap1('R')=0.0; ap1('beta')=0; ap1('I') = I1; ap1('gamma') = 0;
+    ap2 = myfit.allParams{2}; ap2('gamma') = 0;
     myfit.freeParams = {freePrms1,freePrms2};
     Nprms = length(vertcat(myfit.freeParams{:}));% total number of free parameters
-    fmt = 'ENS pattern along [hh0] T=%.2fK H=%.3fT %i params fit';
     label = sprintf(fmt,nDatap94(i).temp,nDatap94(i).field,Nprms);
     fitStr = ['fit'  int2str(lx) 'ICpV' int2str(Nprms) 'rslt']; 
     gofStr = ['gof'  int2str(lx) 'ICpV' int2str(Nprms)];
     [nDatap94(i).(fitStr), nDatap94(i).(gofStr)] = myfit.compute_fit();
-    if mod(i,5)==1% select data to plot
+    if mod(i,1)==0% select data to plot
         myfit.plot_fit(nDatap94(i).(fitStr)); title(label);
 %         xlim([hc-.8 hc+.6]);
     end
     disp(label); disp(nDatap94(i).(fitStr)); disp(nDatap94(i).(gofStr));
+end
+%%
+for i=46%:54
+    label = sprintf(fmt,nDatap94(i).temp,nDatap94(i).field,Nprms);
+    myfit.plot_fit(nDatap94(i).(fitStr)); title(label);
 end
 %%
 np = Nprms;
@@ -81,7 +89,7 @@ for nc=1:np% for each free parameter
     tbl(Ntbl).(fitStrnp).(cfn{nc}) = prm;% store fit parameter value in a new column
     tbl(Ntbl).(fitStrnp).([cfn{nc} '_RelErr']) = relErrm;% and relative error in another new column
 end
-if ~any(strcmp('R',tbl(1).(fitStrnp).Properties.VariableNames))% if there is no field called 'R' in table tbl.(fitStrnp)
+if ~any(strcmp('R',tbl(Ntbl).(fitStrnp).Properties.VariableNames))% if there is no field called 'R' in table tbl.(fitStrnp)
     tbl(Ntbl).(fitStrnp).R = ap1('R')*ones(length(rng),1);% store value of parameter R in another column (this is unnecessary if R is a free parameter)
 end
 rsquarenp = extract_structure_field(nDatap94(rng),gofStrnp,'rsquare');% extract r^2 value from goodness of fit
@@ -119,12 +127,12 @@ for j=1:Navg
 end
 
 %% Identify peak maximum and width
-xM = ones(istart,1);
-fwhm = ones(istart,1);
+xM = ones(length(rng),1);
+fwhm = ones(length(rng),1);
 for i=rng
-    I1 = nDatap94(i).fit3rslt.I;
-    gamma = nDatap94(i).fit3rslt.gamma;
-    x0 = nDatap94(i).fit3rslt.x0;
+    I1 = nDatap94(i).fitStr.I1;
+%     gamma = nDatap94(i).fitStr.gamma;
+    x0 = nDatap94(i).fitStr.x01;
     fnfit = @(x)-I1*voigtIkedaCarpenter_ord(x,[0,140,0,gamma,6.6e-3,0.05,x0]);%fnfit = -1*[fit function] so that the maximum becomes a minimum
     xM(i) = fminbnd(fnfit,hc-.2,hc+.2);% Identify position of the maximum on interval [hc-0.2 hc+0.2]
     M = -fnfit(xM(i));% compute value of the maximum
@@ -151,42 +159,20 @@ end
 %% 2 peak fit
 %% Compute 2 peak fit with 3 free parameters for each peak 
 % and extract splitting between peaks
-xM1 = ones(istart,1); xM2 = ones(istart,1); splitting = zeros(istart,1);
-for i=1:istart
-    label = strcat("T=",num2str(round(nDatap94(i).temp,2)),"K, H=",...
-        num2str(nDatap94(i).field),"T");
-    try
-        if mod(i,10)==1% plot data every 10 values of field
-            [nDatap94(i).fit6rslt, nDatap94(i).gof6] = ENS_peak_fit_ICpV2_function_6params(...
-            nDatap94(i).hh0,nDatap94(i).I,hc,datExcld,1);% compute fit and plot
-            title(strcat("ENS [hh0] cut at ",label," & 6 params fit"))
-            xlim([hc-.75 hc+.6]);
-        else
-            [nDatap94(i).fit6rslt, nDatap94(i).gof6] = ENS_peak_fit_ICpV2_function_6params(...
-            nDatap94(i).hh0,nDatap94(i).I,hc,datExcld);% compute fit but do not plot
-        end
-        disp(strcat(newline+"Fit ICpV1 6 parameters at ",label));
-        disp(nDatap94(i).fit6rslt)
-        I1 = nDatap94(i).fit6rslt.I1; I2 = nDatap94(i).fit6rslt.I2;
-        gamma1 = nDatap94(i).fit6rslt.gamma1; gamma2 = nDatap94(i).fit6rslt.gamma2;
-        x01 = nDatap94(i).fit6rslt.x01; x02 = nDatap94(i).fit6rslt.x02;
-        fnfit1 = @(x)-(I1*voigtIkedaCarpenter_ord(x,[0,140,0,gamma1,6.6e-3,0.05,x01]));
-        fnfit2 = @(x)-(I2*voigtIkedaCarpenter_ord(x,[0,140,0,gamma2,6.6e-3,0.05,x02]));
+xM1 = ones(length(rng),1); xM2 = ones(length(rng),1); splitting = zeros(length(rng),1);
+for i=rng
+    label = sprintf(fmt,nDatap94(i).temp,nDatap94(i).field,Nprms);
+        I1 = nDatap94(i).(fitStr).I1; I2 = nDatap94(i).(fitStr).I2;
+%         gamma1 = nDatap94(i).fitStr.gamma1; gamma2 = nDatap94(i).fitStr.gamma2;
+        x01 = nDatap94(i).(fitStr).x01; x02 = nDatap94(i).(fitStr).x02;
+        fnfit1 = @(x)-(I1*voigtIkedaCarpenter_ord(x,[0,140,0,0,6.6e-3,0.05,x01]));
+        fnfit2 = @(x)-(I2*voigtIkedaCarpenter_ord(x,[0,140,0,0,6.6e-3,0.05,x02]));
     %fnfit = -1*[fit function] so that the maximum becomes a minimum
-        xM1(i) = fminbnd(fnfit1,hc-.2,hc+.2);% Identify position of the maximum on interval [hc-0.2 hc+0.2]
-        xM2(i) = fminbnd(fnfit2,hc-.2,hc+.2);% Identify position of the maximum on interval [hc-0.2 hc+0.2]
+        xM1(i) = fminbnd(fnfit1,xc(1)-.1,xc(1)+.1);% Identify position of the maximum on interval [hc-0.2 hc+0.2]
+        xM2(i) = fminbnd(fnfit2,xc(2)-.1,xc(2)+.1);% Identify position of the maximum on interval [hc-0.2 hc+0.2]
         M1 = -fnfit2(xM1(i));
         M2 = -fnfit2(xM2(i));% compute value of the maximum, to check graphically if the result is correct
-        splitting(i) = xM2(i) - xM1(i);
-    %     if abs(nDatap94(i).fit6rslt.x02)-abs(nDatap94(i).fit6rslt.x01)>0
-    % % if the absolute value of h for the smaller fitted peak is smaller than
-    % % that of the bigger peak, the fit is not accurate anymore
-    %         break 
-    %     end
-    catch % if the fitting process raises an error
-        break
-    end
-    counter = i;% keep track of up to which field the fit was successful
+        splitting(i) = -(xM2(i) - xM1(i))/hc;
 end
 
 %%
