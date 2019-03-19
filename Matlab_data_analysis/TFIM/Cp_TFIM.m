d8afe3abd70d4aa19410a60bb4ff3a261968b67e
Pierre Massat <pmassat@stanford.edu>
2019-03-18 18:49:06 -0700

    Heat capacity v1.3.2. New functions to fit Cp vs T at under *any* transverse field using results from the mean-field Ising model (Stinchcombe 1973).

---
+++ Matlab_data_analysis/TFIM/Cp_TFIM.m
@@ -0,0 +1,21 @@
+function y = Cp_TFIM(t,h)
+% t is the reduced temperature T/Tc
+y = zeros(size(t));
+
+if h==0; tc = 1;
+elseif h<1; tc = h/atanh(h);
+else; tc = 0;
+end
+
+for i=1:length(t)
+    if t(i)<=0
+        y(i) = 0;
+    elseif t(i)<tc
+        r = order_parameter(t(i))./t(i);% ratio of reduced order_parameter to reduced temperature
+        y(i) = r^2.*sech(r)^2 ./ (1 - 1./t(i).*sech(r)^2);% mean-field heat capacity in the ordered phase
+    else
+        r = h./t(i);
+        y(i) = r^2.*sech(r)^2;% mean-field heat capacity in the ordered phase        
+    end
+end
+end
--- Matlab_data_analysis/TFIM/order_parameter.m
+++ Matlab_data_analysis/TFIM/order_parameter.m
@@ -1,6 +1,6 @@
 function y = order_parameter(t)
 %%% calculate the order parameter of the MF FQ transition (which equals the
-%%% normalized pseudospin) as a function of reduced temperature t
+%%% normalized pseudospin) as a function of reduced temperature t = T/Tc
     y = fzero(@(x) x-tanh(x/t), [1e-3 1]);
     % if 0 is included in the range of fzero, y will always be zero, since
     % tanh(0) = 0
