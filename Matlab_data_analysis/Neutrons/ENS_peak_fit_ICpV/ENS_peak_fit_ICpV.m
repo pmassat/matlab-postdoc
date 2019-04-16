9b907847534f15901f065dda378c2c6260201324
Pierre Massat <pmassat@stanford.edu>
2019-04-15 18:47:28 -0700

    Neutrons v1.6.2. Exporting figures for article 'TmVO4_model_nematicity'. Starting w/ Splitting vs H at 0.6K. Also added some notes about the distribution of Schottky anomalies in Cp due to random strains.

--- Matlab_data_analysis/Neutrons/ENS_peak_fit_ICpV/ENS_peak_fit_ICpV.m
+++ Matlab_data_analysis/Neutrons/ENS_peak_fit_ICpV/ENS_peak_fit_ICpV.m
@@ -142,22 +142,25 @@ flag = 0;% reset flag for next run
 
 %% Plot ratio of peak intensities as a function of field
 figure; hold on;
-If1 = Stbl(end).fit2ICpV4.I1; If1Err = Stbl(end).fit2ICpV4.I1_RelErr.*If1;
-If2 = Stbl(end).fit2ICpV4.I2; If2Err = Stbl(end).fit2ICpV4.I2_RelErr.*If2;
-Iratio = If1./If2;
-IrErr = abs(If1Err.*If2-If2Err.*If1)./If2.^2;
-errorbar(Stbl(end).fit2ICpV4.Field_Oe,Iratio,IrErr,'.','MarkerSize',12);
-% errorbar(Stbl.fit2ICpV4.Field_Oe,If1,Stbl.fit2ICpV4.I1_RelErr,'.','MarkerSize',12);
-% errorbar(Stbl.fit2ICpV4.Field_Oe,If2,Stbl.fit2ICpV4.I2_RelErr,'.','MarkerSize',12);
-ylim([0 2*Iratio(1)]);
-title('TmVO$_4$ neutrons elastic 880 peak intensity ratio vs field at 0.6K');
-xlabel('Magnetic field (Oe)'); ylabel('I$_1$/I$_2$');
-nmaxavg = 10;
-annir = annotation('textbox',[0.15 0.8 0.2 0.1],'interpreter','latex',...
-    'String',sprintf('Average of I$_1$/I$_2$ below %.2fT: %.2f',Stbl(end).fit2ICpV4.Field_Oe(nmaxavg),mean(Iratio(1:nmaxavg))),...
-    'FontSize',14,'FontName','Arial','LineStyle','-','EdgeColor','r',...
-    'LineWidth',2,'BackgroundColor',[1 1 1],'Color','k');% add annotation
-annir.FitBoxToText='on';% fit annotation box to text
+if ismember('I1', Stbl(end).(fitStr).Properties.VariableNames) && ismember('I2', Stbl(end).(fitStr).Properties.VariableNames)
+    If1 = Stbl(end).(fitStr).I1; If1Err = Stbl(end).(fitStr).I1_RelErr.*If1;
+    If2 = Stbl(end).(fitStr).I2; If2Err = Stbl(end).(fitStr).I2_RelErr.*If2;
+    Iratio = If1./If2;
+    IrErr = abs(If1Err.*If2-If2Err.*If1)./If2.^2;
+    errorbar(Stbl(end).fit2ICpV4.Field_Oe,Iratio,IrErr,'.','MarkerSize',12);
+    % errorbar(Stbl.fit2ICpV4.Field_Oe,If1,Stbl.fit2ICpV4.I1_RelErr,'.','MarkerSize',12);
+    % errorbar(Stbl.fit2ICpV4.Field_Oe,If2,Stbl.fit2ICpV4.I2_RelErr,'.','MarkerSize',12);
+    ylim([0 2*Iratio(1)]);
+    title('TmVO$_4$ neutrons elastic 880 peak intensity ratio vs field at 0.6K');
+    xlabel('Magnetic field (Oe)'); ylabel('I$_1$/I$_2$');
+    nmaxavg = 10;
+    annir = annotation('textbox',[0.15 0.8 0.2 0.1],'interpreter','latex',...
+        'String',sprintf('Average of I$_1$/I$_2$ below %.2fT: %.2f',Stbl(end).fit2ICpV4.Field_Oe(nmaxavg),mean(Iratio(1:nmaxavg))),...
+        'FontSize',14,'FontName','Arial','LineStyle','-','EdgeColor','r',...
+        'LineWidth',2,'BackgroundColor',[1 1 1],'Color','k');% add annotation
+    annir.FitBoxToText='on';% fit annotation box to text
+else; warning('Need two intensities to plot ratio. Ignoring this part.');
+end
 
 %% Write table to file
 fileChar = [sprintf('%1.eK',nData(1).temp) fitStr '.txt'];% '_R=' sprintf('%2.e',ap1('R'))
@@ -209,7 +212,7 @@ for i=rng
 end
 
 % Estimation of error bars using error bars on peak position from fit
-warning("Check the row number of the table in structure Stbl before running this section!")
+warning('Check the row number of the table in structure Stbl before running this section!')
 j = 1;% check the row number of the table in structure Stbl before running!
 spltRE = Stbl(j).(fitStr).x01_RelErr + Stbl(j).(fitStr).x02_RelErr;
 wghts = min(spltRE)./spltRE;
@@ -255,22 +258,28 @@ sTeff = sprintf('T = %.2f(%.0f)K',Teff,dTeff*1e2);% Effective temperature with e
 sTdr = sprintf('$T_{DR}$=%.1fK',nData(1).temp);
 
 %% Plot fit with data.
+plotRng = field<0.93;
+xPlot = xData(plotRng);
+yPlot = yData(plotRng);
+spltPlot = spltRE(plotRng);
+exclPlot = excludedPoints(plotRng);
 Xfit = linspace(0,max(field),1000);
 Yfit = fitresult(Xfit);% compute fit over a controlled number of points
 figure; hold on
 pfit = plot(Xfit,Yfit,'r-');
-pdat = errorbar(xData,yData,spltRE,'.b','MarkerSize',12,'LineWidth',2);
-pexcl = plot(xData(excludedPoints),yData(excludedPoints),'xm','MarkerSize',12);
-legend([pdat,pexcl,pfit],'splitting vs. field','Excluded','MF fit');
-title('TmVO$_4$ neutrons elastic 880 peak splitting vs field');
+pdat = errorbar(xPlot,yPlot,spltPlot,'.b','MarkerSize',10,'LineWidth',2);
+% pexcl = plot(xPlot(exclPlot),yPlot(exclPlot),'xk','MarkerSize',6);
+% legend([pdat,pexcl,pfit],'Splitting','Excluded','MF fit');
+legend([pdat,pfit],'Splitting','MF fit');
+title('TmVO$_4$ (8 8 0) peak splitting vs field');
 xlabel('H (T)'); ylabel('(a-b)/a$_0$'); grid on
 ylim([0 6e-3]);
 ann2 = annotation('textbox',[0.15 0.3 0.2 0.1],'interpreter','latex',...
-    'String',{'Peak at (8 8 0)' [sTdr '; ' sTeff] strSplit strHc},...
+    'String',{'Peak at (8 8 0)' [sTeff] strSplit strHc},...
     'FontSize',14,'FontName','Arial','LineStyle','-','EdgeColor','r',...
     'LineWidth',2,'BackgroundColor',[1 1 1],'Color','k');% add annotation
 ann2.FitBoxToText='on';% fit annotation box to text
-
+ax = gca; ax.TitleFontSizeMultiplier = 0.8;
 
 %% Estimate critical field at the effective temperature in the absence of demagnetizing factor
 % Function defining the critical field
