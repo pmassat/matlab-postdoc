86c2925922caafb7a693e6f06e43d7f29d074f95
Pierre Massat <pmassat@stanford.edu>
2019-07-15 18:48:51 -0700

    Heat capacity v1.7.1. Cp vs T,H of article 'TmVO4_model_nematicity': computation of Cp in TFIM including longitudinal field.

--- Matlab_data_analysis/TFIM/Cp_TFIM.m
+++ Matlab_data_analysis/TFIM/Cp_TFIM.m
@@ -11,6 +11,7 @@ for i=1:length(t)
     if t(i)<=0
         y(i) = 0;
     elseif t(i)<tc
+%         r = order_parameter(t(i))./t(i);% ratio of reduced order_parameter to reduced temperature
         r = order_parameter(t(i))./t(i);% ratio of reduced order_parameter to reduced temperature
         y(i) = r^2.*sech(r)^2 ./ (1 - 1./t(i).*sech(r)^2);% mean-field heat capacity in the ordered phase
     else
--- Matlab_data_analysis/TFIM/Cp_TFIM_offset_strain.m
+++ Matlab_data_analysis/TFIM/Cp_TFIM_offset_strain.m
@@ -1,4 +1,4 @@
- function y = Cp_TFIM_offset_strain(t,e,h)
+ function y = Cp_TFIM_offset_strain(t,h,e)
 % t is the reduced temperature T/Tc
 y = zeros(size(t));
 
@@ -9,10 +9,12 @@ end
 % tc =1;
 
 for i=1:length(t)
+    x = OP_TFIM(t(i),h,e);
+    r = sqrt((x+e)^2+h^2)./t(i);
     y(i) = 0;
     tr = t(i)/tc;
-    if t(i)<tc || (h==0 && abs(e)>0)
-        r1 = order_parameter_offset_strain(t(i),e)./t(i);
+    if t(i)<tc
+        r1 = r;
         % ratio of reduced order_parameter to reduced temperature
 % Note: the order parameter depends on the ratio t(i)/tc, not on t(i)
         y(i) = r1^2.*sech(r1)^2 ./ (1 - 1./t(i).*sech(r1)^2);% mean-field heat capacity in the ordered phase
@@ -20,20 +22,19 @@ for i=1:length(t)
 %         % ratio of reduced order_parameter to reduced temperature
 % % Note: the order parameter depends on the ratio t(i)/tc, not on t(i)
 %         y(i) = r1^2.*sech(r1)^2 ./ (1 - 1./tr.*sech(r1)^2);% mean-field heat capacity in the ordered phase
-%     else
+    else
 %         r11 =0;
-% %         if abs(e)>0
-% %             r11 = order_parameter_offset_strain(t(i),e)./t(i);
-% %         else; r11=0;
-% %         end
-%         r2 = r11 + h./t(i);
-% %         if e==0
-% %         y(i) = r2^2.*sech(r2)^2;% mean-field heat capacity in the disordered phase
-% %         else
-% %         r3 = (order_parameter_offset_strain(t(i),e))./t(i);% ratio of reduced order_parameter to reduced temperature
-% %         Cord = r3^2.*sech(r3+e)^2 ./ (1 - 1./t(i).*sech(r3+e)^2);% mean-field heat capacity in the ordered phase
-%         y(i) = y(i) + r2^2.*sech(r2)^2;% mean-field heat capacity in the disordered phase            
-% %         end
+%         if abs(e)>0
+%             r11 = order_parameter_offset_strain(t(i),e)./t(i);
+%         else; r11=0;
+%         end
+%         if e==0
+%         y(i) = r2^2.*sech(r2)^2;% mean-field heat capacity in the disordered phase
+%         else
+%         r3 = (order_parameter_offset_strain(t(i),e))./t(i);% ratio of reduced order_parameter to reduced temperature
+%         Cord = r3^2.*sech(r3+e)^2 ./ (1 - 1./t(i).*sech(r3+e)^2);% mean-field heat capacity in the ordered phase
+        y(i) = r^2.*sech(r)^2;% mean-field heat capacity in the disordered phase            
+%         end
     end
 end
 end
